54abaf781dc845c4ffb20063e741dc1bde6afb39
Ivo Reyes <ivoreroman@gmail.com>
2018-12-05 15:45:18 -0600

    Show frame title in Emacs.

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -37,7 +37,6 @@
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
 (setq ns-use-proxy-icon  nil)
-(setq frame-title-format nil)
 
 
 ;; Yasnippet
