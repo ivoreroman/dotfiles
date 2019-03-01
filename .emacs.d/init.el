1911857a2552d750d7ca9ff08c432052d3200975
Ivo Reyes <ivoreroman@gmail.com>
2019-03-01 12:22:15 -0600

    Update emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,4 +1,3 @@
-
 ;; Added by Package.el.  This must come before configurations of
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
@@ -21,10 +20,9 @@
 (setq user-mail-address "ivoreroman@gmail.com")
 
 ;; Themes
-(use-package doom-themes
-	     :ensure t
-	     :config
-	     (load-theme 'doom-nord t))
+;; (use-package zenburn-theme)
+(use-package doom-themes)
+(load-theme 'doom-nord t)
 
 ;; Minimal UI
 (menu-bar-mode   -1)
@@ -38,8 +36,26 @@
 (add-to-list 'default-frame-alist '(ns-appearance . dark))
 (setq ns-use-proxy-icon  nil)
 
+;; Key bindings
+(global-set-key (kbd "M-o") 'other-window)
+
 ;; Sort apropos results by relevancy
 (setq apropos-sort-by-scores t)
+
+;; Scratch
+(setq initial-major-mode 'org-mode)
+(setq initial-scratch-message nil)
+
+;; Ledger
+(use-package ledger-mode
+  :ensure t
+  :custom (ledger-init-load-init-file "~/.ledgerrc"))
+
+;; EditorConfig
+(use-package editorconfig
+  :ensure t
+  :config
+  (editorconfig-mode 1))
 
 ;; Yasnippet
 (use-package yasnippet
@@ -49,6 +65,15 @@
 	'("~/.emacs.d/snippets"))
   (yas-global-mode 1))
 
+;; Markdown
+(use-package markdown-mode
+  :ensure t
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "pandoc"))
+
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -57,8 +82,18 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    (quote
-    ("fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" default)))
- '(package-selected-packages (quote (doom-themes use-package))))
+    ("84890723510d225c45aaff941a7e201606a48b973f0121cb9bcb0b9399be8cba" "93a0885d5f46d2aeac12bf6be1754faa7d5e28b27926b8aa812840fe7d0b7983" "b35a14c7d94c1f411890d45edfb9dc1bd61c5becd5c326790b51df6ebf60f402" "fe666e5ac37c2dfcf80074e88b9252c71a22b6f5d2f566df9a7aa4f9bea55ef8" "4697a2d4afca3f5ed4fdf5f715e36a6cac5c6154e105f3596b44a4874ae52c45" default)))
+ '(ledger-init-load-init-file "~/.ledgerrc" t)
+ '(ledger-reports
+   (quote
+    (("stats" "ledger ")
+     ("bal" "%(binary) -f %(ledger-file) bal")
+     ("reg" "%(binary) -f %(ledger-file) reg")
+     ("payee" "%(binary) -f %(ledger-file) reg @%(payee)")
+     ("account" "%(binary) -f %(ledger-file) reg %(account)"))))
+ '(package-selected-packages
+   (quote
+    (editorconfig yasnippet use-package ledger-mode doom-themes))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
