8cd83571ff3867a1f03b77902234a959de383e30
Ivo Reyes <ivoreyes@uber.com>
2024-04-30 16:40:17 -0700

    Change default Python interpreter

--- .emacs.d/extras/dev.el
+++ .emacs.d/extras/dev.el
@@ -71,6 +71,10 @@
 (use-package json-mode
   :ensure t)
 
+(use-package python-mode
+  :interpreter ("python3" . python-mode))
+
+
 ;; Emacs ships with a lot of popular programming language modes. If it's not
 ;; built in, you're almost certain to find a mode for the language you're
 ;; looking for with a quick Internet search.
