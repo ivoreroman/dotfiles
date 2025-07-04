688b98f48f6c3e5274b0fc4416367b9edfc69ed4
Ivo Reyes Román <2218883+ivoreroman@users.noreply.github.com>
2025-07-03 17:41:58 -0700

    empty work.el

--- .emacs.d/extras/work.el
+++ .emacs.d/extras/work.el
@@ -1,11 +1,1 @@
-;;;;;;;;;;;;;;;
-;;; Uber
-;;;;;;;;;;;;;;;
 
-
-(add-to-list 'auto-mode-alist '("METADATA\\'" . yaml-mode))
-
-(with-eval-after-load 'denote
-  (setq denote-directory (expand-file-name "~/Uber/notes/denote")))
-
-(setq bazel-command '("/Users/ivoreyes/Uber/ios/bazelw"))
